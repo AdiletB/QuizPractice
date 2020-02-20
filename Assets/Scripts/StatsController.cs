4d76cbc1c6531806d7deb3ee7f551fbc02442dc9
AdiletB <a.balabekov@kaznetproject.kz>
2020-02-20 19:45:54 +0600

    Some refactoring

--- Assets/Scripts/StatsController.cs
+++ Assets/Scripts/StatsController.cs
@@ -11,18 +11,19 @@ public class StatsController : MonoBehaviour
     [SerializeField]
     private Button closeButton;
 
-    public void Show(int count){
-        rightAnswers.text = count.ToString();
+    public void Show(int count, int countOfQuestions){
+        rightAnswers.text = GetResultInPercents(countOfQuestions, count).ToString() + "%";
         gameObject.SetActive(true);
     }
+	private double GetResultInPercents(int count, int value){
+		return (value % count) * 100;
+	}
     void Start()
     {
         closeButton.onClick.AddListener(()=> { Application.Quit(); });
     }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+	
+    void OnDestroy(){
+        closeButton.onClick.RemoveAllListeners();
     }
 }
